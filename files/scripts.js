7529b70204efdb05b0fe8647adab3cf16fb6b81f
FroZnShiva <froznshiva@gmx.net>
2015-04-10 15:59:30 +0200

    Add grouping of general user events
    This feature groups general subsequent user events such as joins, parts,
    quits, and nick changes into a group which can be expanded or collapsed.
    
    These events might not be that interesting and appear quite often. In order
    to prevent such events from distracting from the more interesting content
    (chat messages, etc.) one can usually hide such events completely by the
    IRC client. If you don't want to hide them completely you can use this
    feature to reduce them to a single line and be able to show everything by
    just clicking on the group instead of changing the preferences of the IRC
    client.

--- files/scripts.js
+++ files/scripts.js
@@ -1,6 +1,7 @@
 /* Defined in: "Textual 5.app -> Contents -> Resources -> JavaScript -> API -> core.js" */
 
 var mappedSelectedUsers = new Array();
+var groupCount = 0;
 
 Textual.viewBodyDidLoad = function()
 {
@@ -16,6 +17,10 @@ Textual.newMessagePostedToView = function(line)
 	var element = document.getElementById("line-" + line);
 
 	updateNicknameAssociatedWithNewMessage(element);
+
+	if (enableGrouping) {
+		groupGeneralUserEvents(element);
+	}
 }
 
 Textual.nicknameSingleClicked = function(e)
@@ -81,5 +86,138 @@ function userNicknameSingleClickEvent(e)
 				toggleSelectionStatusForNicknameInsideElement(sender[0]);
 			}
 		}
+	}
+}
+
+function isGeneralUserEvent(element)
+{
+	var type = element.getAttribute("command");
+	return ["join", "part", "quit", "nick"].indexOf(type) != -1;
+}
+
+function isGroup(element)
+{
+	var type = element.getAttribute("ltype");
+	return type == "group";
+}
+
+function isMarker(element)
+{
+	return element && element.id && element.id == 'mark';
+}
+
+function groupGeneralUserEvents(currentLine)
+{
+	var previousLine = currentLine.previousElementSibling;
+
+	// Ignore marker line
+	if (isMarker(previousLine))
+		previousLine = previousLine.previousElementSibling;
+
+	if (!isGeneralUserEvent(currentLine) || !previousLine)
+		return;
+
+	var previousType = previousLine.getAttribute("ltype");
+
+	// Don't group if there is just one line.
+	if (!(isGeneralUserEvent(previousLine) || isGroup(previousLine)))
+		return;
+
+	var group = previousLine;
+
+	if (!isGroup(previousLine) && previousType != "general_user_events") {
+		group = createGroup("general_user_events");
+		currentLine.parentElement.insertBefore(group, currentLine);
+	}
+
+	if (!isGroup(previousLine)) {
+		increaseAttribute(group, previousType + "s");
+		addLineToGroup(group, previousLine);
+	}
+
+	// Increase before decorating, else the numbers might be off.
+	var currentType = currentLine.getAttribute("command");
+	increaseAttribute(group, currentType + "s");
+
+	addLineToGroup(group, currentLine);
+}
+
+function createGroup(name)
+{
+	var group = document.createElement("details");
+	group.id = "group-" + groupCount;
+	group.setAttribute("ltype", "group");
+	group.setAttribute("class", "line group");
+	group.setAttribute("gtype", name);
+
+	if (expandNewGroups) {
+		group.setAttribute("open", "");
+	}
+
+	var head = document.createElement("summary");
+	head.id = "group_head-" + groupCount;
+	group.appendChild(head);
+
+	var body = document.createElement("div");
+	body.id = "group_body-" + groupCount;
+	body.setAttribute("class", "group_body");
+	group.appendChild(body);
+
+	var time = document.createElement("span");
+	time.setAttribute("class", "time");
+	head.appendChild(time);
+
+	var message = document.createElement("span");
+	message.setAttribute("class", "message");
+	message.appendChild(document.createTextNode(""));
+	head.appendChild(message);
+
+	groupCount++;
+
+	return group;
+}
+
+function addLineToGroup(group, line)
+{
+	line.parentNode.removeChild(line);
+	group.lastElementChild.appendChild(line);
+
+	var head = group.firstElementChild;
+	var time = line.firstElementChild.firstElementChild.cloneNode(true);
+
+	head.removeChild(head.firstElementChild);
+	head.insertBefore(time, head.firstElementChild);
+
+	decorateGroup(group)
+}
+
+function increaseAttribute(element, name)
+{
+	var value = Number(element.getAttribute(name));
+	element.setAttribute(name, value + 1);
+}
+
+function decorateGroup(group)
+{
+	function stringifyValue(name, title)
+	{
+		if (title == undefined)
+			title = name
+
+		var value = Number(group.getAttribute(name + "s"));
+		return value + " " + (value == 1 ? title : title + "s");
+	}
+
+	var gtype = group.getAttribute("gtype");
+	var head = group.firstElementChild;
+	var message = head.lastElementChild;
+	var text = message.firstChild;
+
+	if (gtype == "general_user_events") {
+		text.textContent = " " +
+			stringifyValue("join") + ", " +
+			stringifyValue("part") + ", " +
+			stringifyValue("quit") + " and " +
+			stringifyValue("nick", "nick change");
 	}
 }
